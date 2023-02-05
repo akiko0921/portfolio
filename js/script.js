52b11264826aae42ee90312003fdda73963b6d94
akiko0921 <as.boc.sa@gmail.com>
2023-02-05 15:27:29 +0900

    portfolio_ogawa

--- js/script.js
+++ js/script.js
@@ -116,7 +116,7 @@ $(window).scroll(function () {
     const windowHeight = $(window).height();
     const scroll = $(window).scrollTop();
 
-    $(".js-fadein").each(function () {
+    $(".js_fadein").each(function () {
         const targetPosition = $(this).offset().top;
         if (scroll > targetPosition - windowHeight + 100) {
             $(this).addClass("is-fadein");
