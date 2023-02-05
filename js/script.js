c025a9aa4b793624e1a9a6c30e3846ea591b8e6e
akiko0921 <as.boc.sa@gmail.com>
2023-02-05 15:18:26 +0900

    ポートフォリオ_ogawa

--- js/script.js
+++ js/script.js
@@ -1,66 +1,64 @@
 $(function () {
     //ここの中に記述
-    $('.wrapper').delay(600).fadeIn(1700);
+    $(".wrapper").delay(600).fadeIn(1700);
 
-
-    
     // トップへボタン
-    let topBtn = $('.top');
+    let topBtn = $(".top");
     topBtn.hide();
     //スクロールが1000に達したらボタン表示
     $(window).scroll(function () {
-    if ($(this).scrollTop() > 1000) {
-        topBtn.fadeIn();
-    } else {
-        topBtn.fadeOut();
-           }
+        if ($(this).scrollTop() > 1000) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
     });
 
     // ハンバーガー
 
-    $('.sp_btn, .sp_nav li').on('click', function () {
-        $('.sp_nav').fadeToggle();
-        $('.sp_btn').toggleClass('open');
-
+    $(".sp_btn, .sp_nav li").on("click", function () {
+        $(".sp_nav").fadeToggle();
+        $(".sp_btn").toggleClass("open");
     });
 
     // デイリースライダー
 
-    $('.daily').slick({
+    $(".daily").slick({
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
         arrows: false,
         autoplay: 1700,
         centerMode: true,
-        centerPadding: '7%',
+        centerPadding: "7%",
         responsive: [
             {
                 breakpoint: 1101,
                 settings: {
                     slidesToShow: 4,
-                }
+                },
             },
             {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 3,
-                }
-            }, {
+                },
+            },
+            {
                 breakpoint: 426,
                 settings: {
                     slidesToShow: 2,
                     // centerMode: false,
-                }
-            }
-        ]
+                },
+            },
+        ],
     });
 
     // ワークススライダー
 
-    $('.works_slider').slick({
+    $(".works_slider").slick({
         centerMode: true,
-        centerPadding: '20%',
+        centerPadding: "20%",
         slidesToShow: 3,
         dots: true,
         autoplay: true,
@@ -74,17 +72,18 @@ $(function () {
                 breakpoint: 1101,
                 settings: {
                     slidesToShow: 3,
-                    centerPadding: '10%',
-                }
-            }, {
+                    centerPadding: "10%",
+                },
+            },
+            {
                 breakpoint: 768,
                 settings: {
                     slidesToShow: 3,
-                    centerPadding: '5%',
-                }
-            }
-        ]
-    })
+                    centerPadding: "5%",
+                },
+            },
+        ],
+    });
 
     const $slider = $(".js-slider");
 
@@ -109,9 +108,6 @@ $(function () {
             }
         });
     });
-
-
-
 });
 
 //  要素がふわっと出てくる
@@ -120,7 +116,7 @@ $(window).scroll(function () {
     const windowHeight = $(window).height();
     const scroll = $(window).scrollTop();
 
-    $('.element').each(function () {
+    $(".js_fadein").each(function () {
         const targetPosition = $(this).offset().top;
         if (scroll > targetPosition - windowHeight + 100) {
             $(this).addClass("is-fadein");
